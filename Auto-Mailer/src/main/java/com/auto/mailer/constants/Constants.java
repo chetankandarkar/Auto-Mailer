78db5d0bc494627f18202c91002c9e7f3d98c0ad
Khan Ibrahim <md.ibrahim.khan98@gmail.com>
2025-07-06 17:22:40 +0530

    reading from gsheets

---
+++ Auto-Mailer/src/main/java/com/auto/mailer/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.auto.mailer.constants;
+
+public class Constants {
+
+	public static final String APPLICATION_NAME = "Ibrahim's Application";
+	public static final String SHEET_RANGE = "Sheet1";
+
+}
